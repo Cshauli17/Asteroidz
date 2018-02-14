238e52462147bc55a281ca7307d404f7af95cc8f
ErinBrophy <erinreedbrophy@gmail.com>
2018-02-14 13:56:14 -0600

    Added a spac object class
    fuck you lil p

---
+++ src/client/SpaceObject.java
@@ -0,0 +1,31 @@
+package client;
+
+import mayflower.Actor;
+
+public class SpaceObject extends Actor{
+
+    public int speed;
+    public int direction;
+
+    public SpaceObject(){
+
+    }
+    public void Vvector(){
+
+    }
+
+    public void Vdirection(){
+
+    }
+    public void ChangeSpeed(){
+
+    }
+    public void ChangeDirection(){
+        
+    }
+    @Override
+    public void act(){
+
+    }
+
+}
