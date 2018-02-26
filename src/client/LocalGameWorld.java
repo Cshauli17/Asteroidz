56b2654341ed207196043c99792c308f03d63083
Chris McLain <christopher@homerunss.com>
2018-02-26 14:17:21 -0600

    field png

--- src/client/LocalGameWorld.java
+++ src/client/LocalGameWorld.java
@@ -3,6 +3,11 @@ package client;
 import mayflower.World;
 
 public class LocalGameWorld extends World {
+
+    public LocalGameWorld() {
+        setBackground("rsrc/finals/field.png");
+    }
+
     @Override
     public void act() {
 
