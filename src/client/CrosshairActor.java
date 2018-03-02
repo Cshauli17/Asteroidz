e39c24df451c6a8994eb0f02a932d17719adcea3
ErinBrophy <erinreedbrophy@gmail.com>
2018-03-02 14:05:44 -0600

    more brady

--- src/client/CrosshairActor.java
+++ src/client/CrosshairActor.java
@@ -1,4 +1,17 @@
 package client;
 
-public class CrosshairActor {
+import mayflower.Actor;
+
+public class CrosshairActor extends Actor {
+
+    public CrosshairActor(String file) {
+       // super(file, 0 , 0);
+
+    }
+
+    @Override
+    public void act(){
+
+    }
+
 }
