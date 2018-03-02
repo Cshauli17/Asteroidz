4bf17d2a81e7166807cbd8153ae5bbd32f8e9a88
Cshauli17 <cshaulis.baseball17@gmail.com>
2018-03-02 13:51:23 -0600

    attempt at movement

--- src/client/LocalGameWorld.java
+++ src/client/LocalGameWorld.java
@@ -6,11 +6,7 @@ public class LocalGameWorld extends World {
 
     public LocalGameWorld() {
         setBackground("rsrc/finals/field.png");
-<<<<<<< HEAD
-        //addObject(new ShipActor("rsrc/Spaceship.png"), 100, 100);
-        //addObject(new Asteroid((int)Math.random()*1 + 5,(int)Math.random()*360,"rsrc/LargeAsteroid.png"),100,100);
-=======
->>>>>>> 2890e97fcdeb89df0b09086128c782346f755442
+
     }
 
     @Override
--- src/client/ShipActor.java
+++ src/client/ShipActor.java
@@ -1,5 +1,8 @@
 package client;
 
+import mayflower.Keyboard;
+import mayflower.Mayflower;
+
 public class ShipActor extends SpaceObject {
 
     public ShipActor(String file) {
@@ -14,7 +17,18 @@ public class ShipActor extends SpaceObject {
 
     @Override
     public void tick() {
-
+        if(Mayflower.isKeyPressed(Keyboard.KEY_W)){
+            Main.client.send("ship:speedup");
+        }
+        else if(Mayflower.isKeyPressed(Keyboard.KEY_S)){
+            Main.client.send("ship:slowdown");
+        }
+        if(Mayflower.isKeyPressed(Keyboard.KEY_A)){
+            Main.client.send("ship:turnleft");
+        }
+        else if(Mayflower.isKeyPressed(Keyboard.KEY_D)){
+            Main.client.send("ship:turnright");
+        }
     }
 
 }
